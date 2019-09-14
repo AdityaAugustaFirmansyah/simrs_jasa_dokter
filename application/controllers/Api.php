12fa1ae48f9801efaff5f17a74c4a10df5810788
AdityaAugustaFirmansyah <38529920+AdityaAugustaFirmansyah@users.noreply.github.com>
2019-09-14 22:21:12 +0700

    restfull

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,8 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 // Don't forget include/define REST_Controller path
-require APPPATH . 'libraries/REST_Controller.php';
-
 /**
  *
  * Controller Api
@@ -18,23 +16,81 @@ require APPPATH . 'libraries/REST_Controller.php';
  * @return    ...
  *
  */
-
+require APPPATH . 'libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
 class Api extends REST_Controller
 {
-    
-  public function __construct($config ='rest')
-  {
-    parent::__construct($config);
-    $this->load->database();
-  }
+
+  function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
 
   public function index_get()
   {
-        $kontak = $this->db->get('tarif_dokter')->result();
-        $this->response($kontak,200);
-      
+    if(!empty($id)){
+      $data = $this->db->get_where("items", ['id' => $id])->row_array();
+  }else{
+      $data = $this->db->get("tarif_dokter")->result();
   }
 
+  $this->response($data, REST_Controller::HTTP_OK);
+  }
+
+  public function index_post()
+  {
+    $params = array(
+      'kd_tarif_dokter' => $this->post('kd_tarif_dokter'),
+      'kd_dokter' => $this->post('kd_dokter'),
+      'no_rawat' => $this->post('no_rawat'),
+      'kd_type_prwtn' => $this->post('kd_type_prwtn'),
+      'perawatan' => $this->post('perawatan'),
+    );
+  
+    $tarif_dokter_id = $this->db->insert('tarif_dokter',$params);
+    if ($tarif_dokter_id) {
+      $this->response($params);
+    }else {
+      $this->response(array('status'=>'fail',502));
+    }
+  }
+
+  public function index_put()
+  {
+    $id = $this->put('kd_tarif_dokter');
+
+    $params = array(
+      'kd_dokter' => $this->put('kd_dokter'),
+      'no_rawat' => $this->put('no_rawat'),
+      'kd_type_prwtn' => $this->put('kd_type_prwtn'),
+      'perawatan' => $this->put('perawatan'),);
+        $this->db->where('kd_tarif_dokter',$id);
+        $update = $this->db->update('tarif_dokter',$params);
+
+        if ($update) {
+          $this->response($params,200);
+        }else {
+          $this->response(array('status'=>'fail',502));
+        }
+  }
+
+  public function index_delete()
+  {
+    $kd_tarif_dokter = $this->delete('tarif_dokter');
+    $this->db->where('kd_tarif_dokter',$kd_tarif_dokter);
+    $delete = $this->db->delete('tarif_dokter');
+    if($delete){
+      $this->response(array('status'=>'succes'),201);
+    }else {
+      $this->response(array('status'=>'fail'),502);
+    }
+  }
+
+  public function countColumn()
+    {
+        return $this->db->count_all('tarif_dokter');
+    }
 }
 
 
